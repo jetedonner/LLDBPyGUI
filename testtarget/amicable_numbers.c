f2d5f9bd38bf45645db57aa94adb8fb68ddfbe10
Kim David Hauser <kim@kimhauser.ch>
2025-07-14 09:04:17 +0200

    Little tidying up and test target amendments

--- testtarget/amicable_numbers.c
+++ testtarget/amicable_numbers.c
@@ -10,15 +10,11 @@
 // Codesign for MacOS
 // codesign --verbose=4 --timestamp --strict --options runtime -s "<YOUR SIGNING CERTIFICATE NAME>" hello_world --force
 
-// Standard include
 #include <stdio.h>
-// For sleep()
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
 #include <string.h>
-
-//time_t start_real_time;// = time(NULL); // Get starting real time
 
 void subfunc(int idx, int var) {
   printf("\n%d -> %d", idx, var);
@@ -32,7 +28,6 @@ int dividerSum(int num2Check){
   {
     int dividend = num2Check % i;
     if(dividend == 0){
-      //printf("\nFound divisor: %d + %d (%d)", i, (num2Check / i), (i + (num2Check / i)));
       nDivSum += i + (num2Check / i);
     }
   }
@@ -41,19 +36,6 @@ int dividerSum(int num2Check){
 
 int main() {
 
-  // // Variable for iteration counter
-  // int idx = 0;
-  
-  // // INT test variable for the disasseblers "Variable" view
-  // int testVar = 1;
-
-  // // Another test variable (char array) for the disasseblers "Variable" view
-  // char hardcoded_string[] = "S3CR3T";
-  
-//start_real_time = time(NULL); // Get starting real time
-  
-  // This msg will prompt the user to enter his / her secret
-  //printf("Welc0me to calc test!\nPlease solve the following calculations:");
   printf("#==================================================#\n");
   printf("| W3lc0m4 to AMICABLE NUMBER GENERATOR             |\n");
   printf("| This app lets you generate amicable number pairs |\n");
@@ -62,13 +44,6 @@ int main() {
   printf("#==================================================#\n");
   printf("\n");
   fflush(stdout);
-
-  // int nVar3 = dividerSum(283);
-  // int nVar1 = dividerSum(220);
-  // int nVar2 = dividerSum(nVar1);
-  // int nVar4 = dividerSum(283);
-  // printf("\nFound amicable number pair: %d / %d / %d / %d ...", nVar1, nVar2, nVar3, nVar4);
-  // return 0;
 
   int searching = 1;
   while(searching)
@@ -89,8 +64,7 @@ int main() {
         if(i % 1000000 == 0){
           // Variable to hold the user input
           char input[256];
-          
-          // This msg will prompt the user to enter his / her secret
+
           printf("Do you want to continue your search? [Y/n]:");
           fflush(stdout);
           // Will wait for user input and store the input in the variable "input"
@@ -99,7 +73,6 @@ int main() {
             break;
           }
         }
-        //printf("\nChecking %d...", i);
         int nDivSum = dividerSum(i);
         if(dividerSum(nDivSum) == i && i < nDivSum){
             printf("\nFound amicable number pair: %d / %d ...", i, nDivSum);
@@ -108,39 +81,13 @@ int main() {
       }
       searching = 0;
     }else{
-      printf("\nThe numbers for the search range are invalid!");
+      printf("\nThe numbers for the search range are invalid! Try again ...");
       fflush(stdout);
     }
   }
+
   printf("\nApp exited normally!");
   fflush(stdout);
-  
-//   while(1)
-//     {
-//       /*if(idx % 3 == 0){
-// //      printf("\n%d", idx);
-//         subfunc(idx, testVar);
-//       }*/
-//       printf("What is the sum of: %d and %d?\n", testVar, (testVar + 1));
-//       fflush(stdout);
-//       // Variable to hold the user input
-//       //char input[256];
-//       int number = 0;
-      
-//       // This msg will prompt the user to enter his / her secret
-//       //printf("Please enter your secret: ");
-      
-//       // Will wait for user input and store the input in the variable "input"
-//       scanf("%d", &number);
-//       if(number == (testVar + (testVar + 1))){
-//         printf("Your result is correct! Proceeding to next calculation ....\n");
-//         testVar *= 2;
-//       }else{
-//         printf("Your result is NOT correct! Try again ....\n");
-//       }
-//       fflush(stdout);
-//       sleep(1);
-//       idx++;
-//     }
+
   return 0;
 }
